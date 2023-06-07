9d5803399150eba1b630243a1a7289a438b2d063
Lauren Miller <lmiller90@fordham.edu>
2023-06-07 17:12:35 -0400

    filled out array questions to align with readmeInterior

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,61 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+/* actually we dont't need inquirer if we are just using an array....
+inquirer
+    .createPromptModule([
+    ])
+*/
+const questions = [
+    {
+        type: 'input',
+        message: 'what is the name of your project?',
+        name: 'projectName'
+    },
+    {
+        type: 'input',
+        message: 'write a description for your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'write out each subject for your table of contents',
+        name: 'TOC'
+    },
+    {
+        type: 'input',
+        message: 'provide your installation instructions',
+        name: 'instructions'
+    },
+    {
+        type: 'input',
+        message: 'provide the proper usage for your project',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'provide the license you would like your project to function under',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'provide guidelines for contributing to your project',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'for test',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'for questions',
+        name: 'input'
+    }
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
