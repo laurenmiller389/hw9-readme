65eac24432bcbe7f136e016d27ea026d0983afc1
Lauren Miller <lmiller90@fordham.edu>
2023-06-07 16:44:13 -0400

    included packages, wrote outliine of readme, inlc fs.writeFile, and initalized app... forgot to commit...

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readmeInterior =
+    `
+        # ${fileName}
+
+        ${data}
+
+        # Description
+
+        # Table of Contents
+
+        # Installation
+
+        # Usage
+
+        # License
+
+        # Contributing
+
+        # Tests
+
+        #Questions
+    `;
+
+    fs.writeFile(`fileName.json`, readmeInterior, utf8, (err) => {
+        if (err) throw err;
+        console.log("data saved")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(fileName, data) {
+    writeToFile(fileName, data);
+}
 
 // Function call to initialize app
 init();
