f1a504abd435f3d35a87025810733c49773ba2ec
Lauren Miller <lmiller90@fordham.edu>
2023-06-08 17:26:14 -0400

    added the interior for renderLicenseLink and renderLicenseSection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -17,11 +17,36 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT License') {
+    return '(https://opensource.org/licenses/MIT)';
+  }
+  else if (license === 'Creative Commons') {
+    return '(http://creativecommons.org/publicdomain/zero/1.0/)'
+  }
+  else if (license === 'GNU') {
+  return '(https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT License') {
+    return 'MIT License';
+  }
+  else if (license === 'Creative Commons') {
+    return 'Creative Commons';
+  }
+  else if (license === 'GNU') {
+    return 'GNU'
+  }
+  else {
+    return '';
+}}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
